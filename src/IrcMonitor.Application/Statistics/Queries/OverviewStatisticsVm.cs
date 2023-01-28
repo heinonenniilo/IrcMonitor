b3924f944b171616c70cb61094e7f5b8198c2c60
Niilo Heinonen <heinonen.niilo@gmail.com>
2023-01-28 17:28:22 +0200

    Backend implementation to get yearly statistics

---
+++ src/IrcMonitor.Application/Statistics/Queries/OverviewStatisticsVm.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using IrcMonitor.Domain.Models;
+
+namespace IrcMonitor.Application.Statistics.Queries;
+public class OverviewStatisticsVm
+{
+    [Required]
+    public List<BarChartRow> Rows { get; set; }
+    [Required]
+    public string ChannelName { get; set; }
+
+}
---
+++ src/IrcMonitor.Domain/Models/BarChartRow.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IrcMonitor.Domain.Models;
+
+public class BarChartRow
+{
+    [Required]
+    public string Label { get; set; }
+    [Required]
+    public int Identifier { get; set; }
+    [Required]
+    public int Value { get; set; }
+}
+
