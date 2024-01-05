02015f7b5b1636c0e9b51ba5ce08b6dcb4340084
Niilo Heinonen <heinonen.niilo@gmail.com>
2024-01-05 22:43:38 +0200

    Fixed issue with year selection, as it was showing years only till year 2023

--- src/IrcMonitor.ReactUi/src/constants/conts.ts
+++ src/IrcMonitor.ReactUi/src/constants/conts.ts
@@ -1,7 +1,9 @@
-export const years = [
-  2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023
-];
+import moment from "moment";
 
+export const years: number[] = Array.from(
+  { length: moment().year() - 2009 + 1 },
+  (_, i) => 2024 - i
+);
 interface PageConfig {
   pageContentMinHeight: string;
 }
